c54fdf205afcd6c7fe50ef2566147f92381b0e33
Kumar Suraj <kumar-suraj@newgen.co.in>
2021-03-28 16:43:46 +0530

    My changes again

--- src/main/java/com/grocery/services/UserDetails.java
+++ src/main/java/com/grocery/services/UserDetails.java
@@ -14,14 +14,15 @@ public interface UserDetails {
         User user = new User(0,"No Data Found","No Data Found","No Data Found","No Data Found");
         User user1 = new User(1,"Kumar Suraj","CTS","Admin","Y");
         User user2 = new User(2,"Himanshu Singh","PAYTM","Normal","Y");
+        User user3 = new User(3,"Ng Suraj CIG","DXC","ibps Master","Y");
         if(userId==1){
             return user1;
         }
         if(userId==2){
             return user2;
         }
-        final User user3 = user;
-        return user3;
+        final User user4 = user;
+        return user4;
     }
 
 
